f0cf772c6f25cd867508acf36b4df6e0d841d026
Cristian Espes <christian_zgza@hotmail.com>
2019-01-09 23:36:50 +0100

    Adición de librerías.
    Migración de librería a AndroidX
    Añadidas librerías para RxJava, RxKotlin y RxAndroid.

--- app/src/main/java/com/cristianespes/todo/MainActivity.kt
+++ app/src/main/java/com/cristianespes/todo/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.cristianespes.todo
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
