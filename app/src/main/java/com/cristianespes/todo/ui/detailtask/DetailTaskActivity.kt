ed8beb62ac8ddc29d8fac481ddc2c666f86dc5db
Cristian Espes <christian_zgza@hotmail.com>
2019-01-23 20:53:10 +0100

    Generado diseño inicial del detalle de la tarea.

--- app/src/main/java/com/cristianespes/todo/ui/detailtask/DetailTaskActivity.kt
+++ app/src/main/java/com/cristianespes/todo/ui/detailtask/DetailTaskActivity.kt
@@ -2,10 +2,13 @@ package com.cristianespes.todo.ui.detailtask
 
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import androidx.core.os.ConfigurationCompat
 import com.cristianespes.todo.R
 import com.cristianespes.todo.data.model.Task
 import com.cristianespes.todo.ui.base.BaseActivity
-import com.cristianespes.todo.ui.edittask.EditTaskFragment
+import kotlinx.android.synthetic.main.activity_detail_task.*
+import java.text.SimpleDateFormat
 
 class DetailTaskActivity : BaseActivity() {
 
@@ -29,5 +32,23 @@ class DetailTaskActivity : BaseActivity() {
 
             Log.d("Patata", "Tarea ${task.content}")
         }
+
+        setupViews()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu to use in the action bar
+        menuInflater.inflate(R.menu.toolbar_items, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    private fun setupViews() {
+        titleDetailTask.text = task.content
+
+        imageDetailTask.setImageResource(R.drawable.ic_task_calendar)
+        dateDetailTask.text = SimpleDateFormat("dd/MM/yyyy", ConfigurationCompat.getLocales(resources.configuration)[0]).format(task.createdAt)
+
+        checkIsDoneDetailTask.isChecked = task.isDone
+        isDoneDetailTask.text = getString(R.string.finished)
     }
 }
